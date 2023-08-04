6c0d05e43ebd40de57e1370018ed5717c95891eb
IDeserveToBeWrited <dodo.coowbooy@gmail.com>
2023-08-04 12:47:21 +0200

    dzialaj

--- utils.sh
+++ utils.sh
@@ -10,11 +10,11 @@ NEXT_VER_CODE=${NEXT_VER_CODE:-$(date +'%Y%m%d')}
 REBUILD=${REBUILD:-false}
 OS=$(uname -o)
 
+DELIM="$(openssl rand -hex 8)"
+
 SERVICE_SH=$(cat scripts/service.sh)
 CUSTOMIZE_SH=$(cat scripts/customize.sh)
 UNINSTALL_SH=$(cat scripts/uninstall.sh)
-
-DELIM="$(openssl rand -hex 8)"
 
 # -------------------- json/toml --------------------
 json_get() { grep -o "\"${1}\":[^\"]*\"[^\"]*\"" | sed -E 's/".*".*"(.*)"/\1/'; }
@@ -437,7 +437,7 @@ build_rv() {
 			fi
 		fi
 		if [ "$build_mode" = apk ]; then
-			local apk_output="${BUILD_DIR}/${app_name_l}-${rv_brand_f}-v${version_f}-${arch_f}-${DELIM}.apk"
+			local apk_output="${BUILD_DIR}/${app_name_l}-${rv_brand_f}-v${version_f}-${arch_f}.apk"
 			cp -f "$patched_apk" "$apk_output"
 			pr "Built ${table} (non-root): '${apk_output}'"
 			continue
